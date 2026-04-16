128b3efaa500bdd371dbf7b140c3b39481f8daf2
Alep <alexob12@gmail.com>
2026-04-16 16:27:45 +0200

    start server on 8001

--- frontend/src/app/main-menu/page.tsx
+++ frontend/src/app/main-menu/page.tsx
@@ -13,10 +13,10 @@ const ottieniApiBaseUrl = () => {
   }
 
   if (typeof window !== "undefined") {
-    return `${window.location.protocol}//${window.location.hostname}:8000`;
+    return `${window.location.protocol}//${window.location.hostname}:8001`;
   }
 
-  return "http://127.0.0.1:8000";
+  return "http://127.0.0.1:8001";
 };
 
 type StatoPagina = "caricamento" | "pronta";
